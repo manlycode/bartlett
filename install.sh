fe294eaf243c7f9958ba408ed95b53ab939680f6
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2015-04-17 12:52:39 -0400

    Tweak the install.sh script.

--- install.sh
+++ install.sh
@@ -3,21 +3,24 @@ PAIR_FULL_NAME=${PAIR_FULL_NAME:-"Garry Pair"}
 PAIR_GROUP=${PAIR_GROUP:-"pair"}
 CURRENT_USERNAME=`whoami`
 
-
 echo "Installing github-auth..."
 bundle install --binstubs
-
-echo "\n\nCreating the group ${PAIR_GROUP}"
-sudo dscl . -create /Groups/${PAIR_GROUP} RecordName "Pair"
 
 echo "\n\nCreating the user ${PAIR_USERNAME}..."
 sudo dscl . -append /Users/${PAIR_USERNAME} RecordName '$PAIR_FULL_NAME' $PAIR_USERNAME
 sudo mkdir -p /Users/pair/.ssh
 sudo chown -R pair /Users/pair
 
-sudo dscl . append /Groups/${PAIR_GROUP} GroupMembership ${PAIR_USERNAME}
-sudo dscl . append /Groups/${PAIR_GROUP} GroupMembership ${CURRENT_USERNAME}
+echo "\n\nCreating the group ${PAIR_GROUP}"
+sudo dscl . create /Groups/pair
+sudo dscl . create /Groups/pair RealName "pair"
+sudo dscl . create /Groups/pair passwd "*"
+sudo dscl . create /Groups/pair gid 400
+sudo dscl . create /Groups/pair GroupMembership ${PAIR_USERNAME}
+sudo dscl . append /Groups/pair GroupMembership ${CURRENT_USERNAME}
 
+sudo chgrp -R pair /Users/pair/.ssh
 
 echo "Updating sshd_config..."
 sudo sed -E -i.bak 's/^#?(PasswordAuthentication|ChallengeResponseAuthentication).*$/\1 no/' /etc/sshd_config
+
