e299f79b3d01eac276bfe3271d847553afecc335
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2015-04-21 12:50:28 -0400

    Correct the install script.

--- install.sh
+++ install.sh
@@ -2,27 +2,43 @@
 
 PAIR_USERNAME=${PAIR_USERNAME:-"pair"}
 PAIR_FULL_NAME=${PAIR_FULL_NAME:-"Garry Pair"}
+PAIR_HOME="/Users/${PAIR_USERNAME}"
+
 PAIR_GROUP=${PAIR_GROUP:-"pair"}
 CURRENT_USERNAME=`whoami`
 
 echo "Installing github-auth..."
 bundle install --binstubs
 
-echo "\n\nCreating the user ${PAIR_USERNAME}..."
-sudo dscl . -append /Users/${PAIR_USERNAME} RecordName '$PAIR_FULL_NAME' $PAIR_USERNAME
-sudo mkdir -p /Users/pair/.ssh
-sudo chown -R pair /Users/pair
+echo "Creating the user ${PAIR_USERNAME}..."
+sudo mkdir ${PAIR_HOME}
+sudo dscl . create /Users/${PAIR_USERNAME}
+sudo dscl . create /Users/${PAIR_USERNAME} RealName ${PAIR_FULL_NAME}
+sudo dscl . create /Users/${PAIR_USERNAME} UserShell /bin/bash
+sudo dscl . create /Users/${PAIR_USERNAME} UniqueID 1337
+sudo dscl . create /Users/${PAIR_USERNAME} PrimaryGroupID 1000
+sudo dscl . create /Users/${PAIR_USERNAME} PASSWORD
+sudo dscl . create /Users/${PAIR_USERNAME} NFSHomeDirectory ${PAIR_HOME}
 
 echo "\n\nCreating the group ${PAIR_GROUP}"
-sudo dscl . create /Groups/pair
-sudo dscl . create /Groups/pair RealName "pair"
-sudo dscl . create /Groups/pair passwd "*"
-sudo dscl . create /Groups/pair gid 400
-sudo dscl . create /Groups/pair GroupMembership ${PAIR_USERNAME}
-sudo dscl . append /Groups/pair GroupMembership ${CURRENT_USERNAME}
+sudo dscl . create /Groups/${PAIR_GROUP}
+sudo dscl . create /Groups/${PAIR_GROUP} RealName ${PAIR_GROUP}
+sudo dscl . create /Groups/${PAIR_GROUP} passwd "*"
+sudo dscl . create /Groups/${PAIR_GROUP} gid 400
+sudo dscl . create /Groups/${PAIR_GROUP} GroupMembership ${PAIR_USERNAME}
+sudo dscl . append /Groups/${PAIR_GROUP} GroupMembership ${CURRENT_USERNAME}
 
-sudo chgrp -R pair /Users/pair/.ssh
+sudo chown -R ${PAIR_USERNAME}:${PAIR_GROUP} ${PAIR_HOME}
+
+sudo -u ${PAIR_USERNAME} mkdir -p ${PAIR_HOME}/.ssh
+sudo -u ${PAIR_USERNAME} touch ${PAIR_HOME}/.ssh/authorized_keys
+sudo -u ${PAIR_USERNAME} chmod g+w ${PAIR_HOME}/.ssh/authorized_keys
+
+sudo chgrp -R pair /Users/${PAIR_USERNAME}/.ssh
 
 echo "Updating sshd_config..."
 sudo sed -E -i.bak 's/^#?(PasswordAuthentication|ChallengeResponseAuthentication).*$/\1 no/' /etc/sshd_config
 
+echo "Symlinking default socket file.."
+sudo ln ~/.tmux.socket ${PAIR_HOME}/.tmux.socket
+sudo chown ${CURRENT_USERNAME}:${PAIR_GROUP} ${PAIR_HOME}/.tmux.socket
